9db47308946f0f68db511d9d64841854d4da4acc
delossantosk0252 <159347811+delossantosk0252@users.noreply.github.com>
2024-03-18 12:47:39 +1300

    colour list demo

---
+++ 01_list_demo.py
@@ -0,0 +1,11 @@
+some_colors = [
+    ['aliceblue', '#F0F8FF', '19', 'black'],
+    ['antiquewhite', '#FAEBD7', '20', 'black'],
+    ['antiquewhite1', '#FFEFBD', '20', 'black'],
+    ['antiquewhite2', '#EEDFCC', '19', 'black'],
+    ['antiquewhite3', '#CDCOBO', '16', 'black'],
+    ['antiquewhite4', '#8B8378', '11', 'black']
+]
+
+for item in some_colors:
+    print(item[0])
